ce94ddb0153265814881bc072cbbbeb295389dcc
abdulrazak <razlaw5@gmail.com>
2023-03-17 18:43:00 +0100

    add new fontstyle and refactor home content

--- my-switch/src/pages/Home.tsx
+++ my-switch/src/pages/Home.tsx
@@ -1,22 +1,14 @@
-import { styled, Typography } from '@mui/material';
-import { useContext } from 'react';
+import { Typography } from '@mui/material';
 import { Helmet } from 'react-helmet';
-
-import logo from '../logo.svg';
-
-import { AppContext } from '../contexts';
 import { APP_TITLE, PAGE_TITLE_HOME } from '../utils/constants';
 import { Module } from '../components/Module';
 import { Hero } from '../components/Hero';
 import AlertDialogue from '../components/AlertDialogue';
 
-export const Home = () => {
-  const context = useContext(AppContext);
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
 
-  // const cardContainer = {
-  //   display: 'flex',
-  //   margin: '0 auto' 
-  // }
+export const Home = () => {
 
   return (
     <>
@@ -29,53 +21,51 @@ export const Home = () => {
         <Hero/>
         <AlertDialogue />
       </Module>
-      <Typography variant="h4">{`Hello, ${context.user.name} 🎃`}</Typography>
-      <LogoWrapper>
-        <StyledLogo src={logo} alt="logo" />
-      </LogoWrapper>
-      <h1>Hello world</h1>
-      <p>We could spend days or even months only talking about TypeScript and React,
-       guys. There is plenty to explore, learn, and practice. My intention when 
-       building this starter application is to cover some basic topics, save us some 
-       time on future posts' exercises, and provide you, followers, with a step ahead 
-       when starting your own apps. Hopefully, it'll be a mission accomplished 😃
-      If I may suggest to you what is next, it would be...practice practice practice! 
-      Start React TypeScript applications from scratch, explore the long list of 
-      components on the Material-UI web page, and combine them, play with the React router.
-       The possibilities are many, and it only depends on you to crush them!</p>
-       <Module background='orange'> 
-       <p>We could spend days or even months only talking about TypeScript and React,
-       guys. There is plenty to explore, learn, and practice. My intention when 
-       building this starter application is to cover some basic topics, save us some 
-       time on future posts' exercises, and provide you, followers, with a step ahead 
-       when starting your own apps. Hopefully, it'll be a mission accomplished 😃
-      If I may suggest to you what is next, it would be...practice practice practice! 
-      Start React TypeScript applications from scratch, explore the long list of 
-      components on the Material-UI web page, and combine them, play with the React router.
-       The possibilities are many, and it only depends on you to crush them!</p>
-
-         </Module>
+         <PageContent/>
     </>
   );
 };
 
-const LogoWrapper = styled('div')`
-  text-align: center;
-  margin-top: 6rem;
-`;
+// const LogoWrapper = styled('div')`
+//   text-align: center;
+//   margin-top: 6rem;
+// `;
 
-const StyledLogo = styled('img')`
-  height: 40vmin;
-  pointer-events: none;
-  @media (prefers-reduced-motion: no-preference) {
-    animation: App-logo-spin infinite 15s linear;
-  }
-  @keyframes App-logo-spin {
-    from {
-      transform: rotate(0deg);
-    }
-    to {
-      transform: rotate(360deg);
-    }
-  }
-`;
+const PageContent = ()=>{
+
+return(
+  <Box  sx={{ flexGrow: 1 }}>
+      <Grid container justifyContent='center' spacing={8}>
+        <Grid item xs={12} md={3.5}>
+          <Typography variant='h5' mb={2}> Descriptive</Typography>
+        <Typography variant='body1'>
+        React makes it painless to create interactive UIs. Design simple views for each state in your application, and React will efficiently update and render just the right components when your data changes.
+<span><br /></span><span><br /></span>
+Declarative views make your code more predictable and easier to debug
+        </Typography>
+       
+        </Grid>
+        <Grid item xs={12} md={3.5}>
+        <Typography variant='h5' mb={2}> Descriptive</Typography>
+          <Typography variant='body1'>
+          Build encapsulated components that manage their own state, then compose them to make complex UIs.
+        <span><br /></span><span><br /></span>
+
+Since component logic is written in JavaScript instead of templates, you can easily pass rich data through your app and keep state out of the DOM.
+          </Typography>
+
+        </Grid>
+        <Grid item xs={12} md={3.5}>
+        <Typography variant='h5' mb={2}> Descriptive</Typography>
+          <Typography variant='body1'>
+          We don’t make assumptions about the rest of your technology stack, so you can develop new features in React without rewriting existing code.
+        <span><br /></span><span><br /></span>
+React can also render on the server using Node and power mobile apps using...
+          </Typography>
+
+        </Grid>
+      </Grid>
+    </Box>
+  )
+
+}
