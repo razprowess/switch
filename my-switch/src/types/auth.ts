ba76b8f4da30a02cee35d3e94ca272fd7270ad14
abdulrazak <razlaw5@gmail.com>
2023-05-09 11:34:03 +0100

    refactor signup page to use formik and yup

--- my-switch/src/types/auth.ts
+++ my-switch/src/types/auth.ts
@@ -2,3 +2,13 @@ export interface ILoginFormValues {
     email?: string
     password?: string
   }
+
+  export interface ISignUpFormValues {
+    firstname?: string
+    lastname?: string
+    username?: string
+    email?: string
+    password?: string
+    repeatpassword?: string
+    profession?: string
+  }
